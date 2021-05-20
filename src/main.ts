90044c75b0bfe646af50de94934d2ec961164cdd
pixel <303176530@qq.com>
2021-05-20 15:15:30 +0800

    增加空壳

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "@/router"
 import store from "@/store"
+import "@/permission"
 import { ElButton, ElSelect,ElInput,ElForm,ElFormItem } from 'element-plus';
 import 'element-plus/packages/theme-chalk/src/base.scss';
 
