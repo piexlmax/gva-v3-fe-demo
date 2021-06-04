37b4f766ad0cc0fe4a2b3a54abf94f1918c4d032
pixel <303176530@qq.com>
2021-06-04 17:41:26 +0800

    为axios增加返回定义

--- src/api/test.ts
+++ src/api/test.ts
@@ -1,7 +1,7 @@
 import service from "@/utils/request"
-import {requestT} from "@/types/service/index"
+import {requestModel} from "@/types/service/index"
 
-export const test:requestT = () => service({
+export const test:requestModel = () => service({
     url: "http://www.baidu.com",
     method: "GET"
 })
